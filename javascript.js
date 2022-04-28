b24a55ab9d30b1568633d019e7696e7a3736cc5e
marioavaj <predaj@predaj-tonerov.sk>
2022-04-28 11:19:38 +0200

    first commit

---
+++ javascript.js
@@ -0,0 +1,33 @@
+ for(i = 1990; i <=2030; i++) {
+    let year = i;
+    let leap = isLeapYear(year);
+
+    if (leap == 1){
+        if (leap == 1  &&  year%10==0){
+            if (leap == 1  &&  year%10==0 && year==2000){
+                console.log("P O N " + year)
+            }else console.log( "P O " + year)
+        } else console.log( "P " + year)
+    } else {
+    if(year%10==0){
+        console.log("O " + year)
+    }else console.log(year)
+}
+ }
+ 
+ function isLeapYear(year){
+ 
+    if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    return 1;
+                }
+                return 0;
+            }
+            return 1;
+        }
+        return 0;
+    }
+
+
+
